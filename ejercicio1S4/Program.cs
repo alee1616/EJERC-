9d7a94c8c9d6ebe34af32b0b0c819013645a5f29
alee1616 <maguillenc@uamv.edu.ni>
2025-09-01 20:35:40 -0600

    ejercicios4

--- ejercicio1S4/Program.cs
+++ ejercicio1S4/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int edad = 20;
-if (edad >= 18 || edad < 65)
+int i = 0;
+while (i < 5)
 {
-    Console.WriteLine("Edad válida para trabajar");
-}
+    Console.WriteLine("Numero: " + i);
+    //i++; <- Incrementar 'i' para evitar un bucle infinito
+        }
+
+
+// El problema con este código es que es un bucle infinito,
+// ya que la variable 'i' nunca se incrementa.
+// La solución es agregar 'i++' dentro del bucle a como lo indica
+// el comentario dentro del bloque de código
+
