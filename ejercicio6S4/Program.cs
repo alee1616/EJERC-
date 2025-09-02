fcc18097fe65c5c240c7361b7200ac688763e3b5
alee1616 <maguillenc@uamv.edu.ni>
2025-09-02 12:24:56 -0600

    EJRCICIOS

---
+++ ejercicio6S4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int edad = 17;
+// Se solicita al usuario que ingrese su edad
+Console.Write("Ingresa tu edad: ");
+        
+// Se lee el valor ingresado por el usuario y se convierte a entero
+edad = Convert.ToInt32(Console.ReadLine());
+
+
+if (edad >= 18)
+{
+    Console.WriteLine("Es mayor de edad.");
+    Console.WriteLine("// Puede votar.// ");
+}
+else
+{
+    Console.WriteLine("Es menor de edad.");
+}
+// En el código original, faltaban las llaves {} después del if y el else.
+// Sin llaves, solo la primera línea después del if o else se considera parte de ese bloque.
+// Esto provocaba que "Console.WriteLine("Puede votar.");" se ejecutara siempre, aunque la condición no se cumpliera.
+
+// Se agregaron llaves para agrupar correctamente las instrucciones que deben ejecutarse
+// solo si la condición se cumple o no se cumple.
