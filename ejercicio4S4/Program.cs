547c45850ba0d4f6edb3ca5100b0c45d2014b310
alee1616 <maguillenc@uamv.edu.ni>
2025-09-01 23:40:12 -0600

    ejercicio

---
+++ ejercicio4S4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero = 10;
+bool esMayor;
+ Console.Write("Ingrese un número: ");
+ numero = Convert.ToInt32(Console.ReadLine());
+
+// ERROR: aquí se usó '=' (asignación) en lugar de un operador de comparación.
+// esMayor = (numero = 5);
+
+// SOLUCIÓN: usar '>' para comprobar si es mayor que 5
+esMayor = (numero > 5);
+
+Console.WriteLine("¿Es mayor que 5? " + esMayor);
