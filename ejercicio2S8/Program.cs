1db973c4e71197d876c0fd5886882e8be35ab763
alee1616 <maguillenc@uamv.edu.ni>
2025-09-24 09:52:01 -0600

    commit ejercicio2S8

---
+++ ejercicio2S8/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class VariablesEjemplos
+{
+    static int contador = 0; // ??? Global o Loal
+    static void Main()
+    {
+        int numero = 10; // ??? global o local??
+        contador ++;
+        Console.WriteLine($"Numero: {numero}, Contador: {contador}");
+        OtraFuncion();
+    }
+    static void OtraFuncion()
+    {
+        string mensaje = "Hola"; // ??? global o local??
+        contador++;
+        Console.WriteLine($"Mensaje: {mensaje}, Contador: {contador}");
+    }
+        
+}
