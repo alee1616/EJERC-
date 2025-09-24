539a88008e400e09bb915e17ac56fd61cddd43be
alee1616 <maguillenc@uamv.edu.ni>
2025-09-24 09:38:50 -0600

    commit ejercicio18

---
+++ ejercicio1S8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class VariablesEjemplos
+{
+
+    static void Main()
+    {
+        int numero = 15; //Variable local main
+        Console.WriteLine($"Variable local en Main: {numero}");
+        MostrarMensaje();
+    }
+    static void MostrarMensaje()
+    {
+        string mensaje = "Hola desde otra función";//Variable local MostrarMensaje
+        Console.WriteLine(mensaje);
+    }
+}
