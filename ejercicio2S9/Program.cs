c0ce054ce182877d139da14993f5c95cfd1a0c90
alee1616 <maguillenc@uamv.edu.ni>
2025-10-01 09:58:14 -0600

    commit

--- ejercicio2S9/Program.cs
+++ ejercicio2S9/Program.cs
@@ -17,7 +17,9 @@ class Mascota
     //con public Mascota(string nombre, string tipo, int energia) se podría inicializar la energía también
     //este devuelve un objeto de tipo Mascota
     {
-        this.nombre = nombre;   // "this" se usa para diferenciar entre el parámetro y el atributo
+        this.nombre = nombre;
+        // "this" se usa para diferenciar entre el parámetro y el atributo
+        // this es para referirse al objeto actual, en este caso al objeto Mascota que se está creando.
         this.tipo = tipo;
         this.energia = 100;     // todas las mascotas empiezan con energía al 100
     }
@@ -58,23 +60,29 @@ class Mascota
     }
 }
 
-// Clase principal Program
+// Clase principal Programa
 // Aquí está el método Main que es el punto de entrada del programa.
+// CREACIÓN DE OBJETOS (MASCOTAS)
+// Usamos la clase Mascota para crear dos objetos distintos:
+//los dos objetos creados son independientes, cada uno tiene sus propios atributos
+//en este caso son perro y gato
 class Program
 {
     static void Main(string[] args)
-    // el static se usa para que no haga falta crear un objeto de la clase Program para llamar a Main
     {
         // CREACIÓN DE OBJETOS (MASCOTAS)
         // Usamos la clase Mascota para crear dos objetos distintos:
-        //los dos objetos creados son independientes, cada uno tiene sus propios atributos
-        //en este caso son perro y gato
+
         Mascota perro = new Mascota("Firulais", "Perro");  
         Mascota gato = new Mascota("Misu", "Gato");
+        Console.WriteLine();  // Línea en blanco para separar
 
+        Console.WriteLine(">>>>>> ESTADO INICIAL <<<<<<");
         // ESTADO INICIAL DE LAS MASCOTAS
         perro.Estado();
         gato.Estado();
+
+        Console.WriteLine("\n////// ACCIONES DE FIRULAIS //////");
 
         // ACCIONES CON EL PERRO
         perro.Jugar(3);       // Firulais juega 3 minutos → gasta 30 de energía
@@ -84,15 +92,19 @@ class Program
 
         Console.WriteLine();  // Línea en blanco para separar
 
+
+        Console.WriteLine("\n////// ACCIONES DE FIRULAIS //////");
         // ACCIONES CON EL GATO
         gato.Jugar(5);        // Misu juega 5 minutos → gasta 50 de energía
         gato.Estado();        // Estado después de jugar
         gato.Alimentar(15);   // Misu come y recupera +15 de energía
         gato.Estado();        // Estado actualizado
 
+
         // FIN DEL PROGRAMA
         // Console.ReadLine se usa para que la ventana de la consola
         // no se cierre automáticamente al terminar.
+        Console.WriteLine("\n****** FIN DEL PROGRAMA ******");
         Console.ReadLine();
     }
 }
