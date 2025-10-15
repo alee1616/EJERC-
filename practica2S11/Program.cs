dd87109d7bd8b639e408cbff7faf856d67efc616
alee1616 <maguillenc@uamv.edu.ni>
2025-10-15 09:45:03 -0600

    commit

--- practica2S11/Program.cs
+++ practica2S11/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Stack<int> numeros = new Stack<int>();
+numeros.Push(10);
+numeros.Push(20);
+numeros.Push(30);
+
+Console.WriteLine("Elemento superior: " + numeros.Peek());
+numeros.Pop(); 
+Console.WriteLine("Elementos restantes: " + numeros.Peek());
