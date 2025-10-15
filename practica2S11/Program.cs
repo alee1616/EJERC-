c5989a8ab4940ce2b8117e4b8069cd10dfe980d1
alee1616 <maguillenc@uamv.edu.ni>
2025-10-15 09:46:27 -0600

    commit

--- practica2S11/Program.cs
+++ practica2S11/Program.cs
@@ -5,5 +5,10 @@ numeros.Push(20);
 numeros.Push(30);
 
 Console.WriteLine("Elemento superior: " + numeros.Peek());
-numeros.Pop(); 
+numeros.Pop();
 Console.WriteLine("Elementos restantes: " + numeros.Peek());
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
