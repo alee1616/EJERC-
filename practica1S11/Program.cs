4fd59713e1e4e719879e561465e204d78eee06a3
alee1616 <maguillenc@uamv.edu.ni>
2025-10-15 09:39:11 -0600

    COMMIT

--- practica1S11/Program.cs
+++ practica1S11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ejemplo de funcionamiento de las filas 
+
+
+string[] palabras = new string[3];
+
+Stack<int> pila = new Stack<int>();
+palabras.Push("uno");
+palabras.Push("dos");
+palabras.Push("tres");
+
+while (pila.Count > 0)
+{
+    Console.WriteLine(pila.Pop());
+}
