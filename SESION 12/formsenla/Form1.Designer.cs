58e3584f0f07d55e19d0cab1b145ac41e2ffa11e
alee1616 <maguillenc@uamv.edu.ni>
2025-11-17 09:56:33 -0600

    form de visual

---
+++ SESION 12/formsenla/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace formsenla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.otrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.escribaAquíToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sumarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiplicarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.divisiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 33);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.otrosToolStripMenuItem,
+            this.escribaAquíToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // otrosToolStripMenuItem
+            // 
+            this.otrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sumarToolStripMenuItem,
+            this.restarToolStripMenuItem,
+            this.multiplicarToolStripMenuItem,
+            this.divisiónToolStripMenuItem});
+            this.otrosToolStripMenuItem.Name = "otrosToolStripMenuItem";
+            this.otrosToolStripMenuItem.Size = new System.Drawing.Size(127, 29);
+            this.otrosToolStripMenuItem.Text = "Operaciones";
+            this.otrosToolStripMenuItem.Click += new System.EventHandler(this.otrosToolStripMenuItem_Click);
+            // 
+            // escribaAquíToolStripMenuItem
+            // 
+            this.escribaAquíToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.escribaAquíToolStripMenuItem.Name = "escribaAquíToolStripMenuItem";
+            this.escribaAquíToolStripMenuItem.Size = new System.Drawing.Size(79, 29);
+            this.escribaAquíToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // sumarToolStripMenuItem
+            // 
+            this.sumarToolStripMenuItem.Name = "sumarToolStripMenuItem";
+            this.sumarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.sumarToolStripMenuItem.Text = "Sumar ";
+            this.sumarToolStripMenuItem.Click += new System.EventHandler(this.sumarToolStripMenuItem_Click);
+            // 
+            // restarToolStripMenuItem
+            // 
+            this.restarToolStripMenuItem.Name = "restarToolStripMenuItem";
+            this.restarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.restarToolStripMenuItem.Text = "Restar";
+            this.restarToolStripMenuItem.Click += new System.EventHandler(this.restarToolStripMenuItem_Click);
+            // 
+            // multiplicarToolStripMenuItem
+            // 
+            this.multiplicarToolStripMenuItem.Name = "multiplicarToolStripMenuItem";
+            this.multiplicarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.multiplicarToolStripMenuItem.Text = "Multiplicar ";
+            this.multiplicarToolStripMenuItem.Click += new System.EventHandler(this.multiplicarToolStripMenuItem_Click);
+            // 
+            // divisiónToolStripMenuItem
+            // 
+            this.divisiónToolStripMenuItem.Name = "divisiónToolStripMenuItem";
+            this.divisiónToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.divisiónToolStripMenuItem.Text = "División";
+            this.divisiónToolStripMenuItem.Click += new System.EventHandler(this.divisiónToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.AutoToolTip = true;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.ToolTipText = "Cerrar la aplicación";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem escribaAquíToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sumarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiplicarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem divisiónToolStripMenuItem;
+    }
+}
+
---
+++ SESION 12/formsenla/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace formsenla
+{
+    public partial class Form1 : Form
+    {
+        private Label lblResultado;
+        private TextBox txtA;
+        private TextBox txtB;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblResultado = new Label();
+            lblResultado.Location = new Point(10, 100);
+            lblResultado.Size = new Size(100, 23);
+            lblResultado.Name = "lblResultado";
+            this.Controls.Add(lblResultado);
+
+            txtA = new TextBox();
+            txtA.Location = new Point(10, 20);
+            txtA.Size = new Size(100, 23);
+            txtA.Name = "txtA";
+            this.Controls.Add(txtA);
+
+            txtB = new TextBox();
+            txtB.Location = new Point(10, 50);
+            txtB.Size = new Size(100, 23);
+            txtB.Name = "txtB";
+            this.Controls.Add(txtB);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void otrosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ejemplo de menu, menu en c#");
+        }
+
+        // Normalize input and try multiple parsing strategies to avoid false "empty" or culture issues.
+        private bool TryGetInputs(out double a, out double b)
+        {
+            a = 0;
+            b = 0;
+
+            // Raw texts as entered
+            string rawA = txtA.Text ?? string.Empty;
+            string rawB = txtB.Text ?? string.Empty;
+
+            // Remove common invisible whitespace (non-breaking space etc.) and trim
+            rawA = NormalizeWhitespace(rawA);
+            rawB = NormalizeWhitespace(rawB);
+
+            if (string.IsNullOrWhiteSpace(rawA))
+            {
+                MessageBox.Show("Ingrese un valor para A.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtA.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(rawB))
+            {
+                MessageBox.Show("Ingrese un valor para B.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtB.Focus();
+                return false;
+            }
+
+            // Try parsing using several strategies:
+            // 1) Current culture
+            // 2) Invariant culture
+            // 3) Swap common decimal separators (useful if user typed '.' but culture expects ',')
+            var culture = CultureInfo.CurrentCulture;
+            var styles = NumberStyles.Float | NumberStyles.AllowThousands;
+
+            if (double.TryParse(rawA, styles, culture, out a) &&
+                double.TryParse(rawB, styles, culture, out b))
+            {
+                return true;
+            }
+
+            // Try invariant (accepts '.' decimal)
+            if (double.TryParse(rawA, styles, CultureInfo.InvariantCulture, out a) &&
+                double.TryParse(rawB, styles, CultureInfo.InvariantCulture, out b))
+            {
+                return true;
+            }
+
+            // Try swapping separators heuristically
+            string swappedA = SwapDecimalSeparators(rawA, culture);
+            string swappedB = SwapDecimalSeparators(rawB, culture);
+
+            if (double.TryParse(swappedA, styles, culture, out a) &&
+                double.TryParse(swappedB, styles, culture, out b))
+            {
+                return true;
+            }
+
+            // If any parse failed, show a clear message mentioning the raw values and culture
+            string message = string.Format("Valores inválidos para la cultura {0}.\nA: '{1}'\nB: '{2}'", culture.Name, rawA, rawB);
+            MessageBox.Show(message, "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            // Focus the first invalid control
+            if (!double.TryParse(rawA, styles, culture, out _))
+                txtA.Focus();
+            else
+                txtB.Focus();
+
+            return false;
+        }
+
+        // Replace NBSP and other non-standard spaces, then trim
+        private static string NormalizeWhitespace(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            // Replace common non-breaking spaces with normal space
+            s = s.Replace('\u00A0', ' ');
+            s = s.Replace('\u2007', ' ');
+            s = s.Replace('\u202F', ' ');
+            // Remove other control chars except digits, signs, separators, E/e
+            // Keep it simple: trim and collapse inner control chars
+            var sb = new StringBuilder(s.Length);
+            foreach (char c in s)
+            {
+                if (!char.IsControl(c))
+                    sb.Append(c);
+            }
+            return sb.ToString().Trim();
+        }
+
+        // If culture decimal is ',' and text contains '.', swap '.' -> ',' (and viceversa).
+        private static string SwapDecimalSeparators(string input, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            string dec = culture.NumberFormat.NumberDecimalSeparator;
+            string other = dec == "," ? "." : ",";
+
+            // If input already uses culture decimal, return as-is
+            if (input.Contains(dec) && !input.Contains(other))
+                return input;
+
+            // If input contains the other separator, replace it with culture separator.
+            // Also remove thousands separators that conflict.
+            string thousandSep = culture.NumberFormat.NumberGroupSeparator;
+            string otherThousand = thousandSep == "," ? "." : ",";
+
+            string temp = input.Replace(other, dec);
+
+            // If both separators are present (ambiguous), remove group separators (both varieties)
+            temp = temp.Replace(thousandSep, string.Empty).Replace(otherThousand, string.Empty);
+
+            return temp;
+        }
+
+        private void sumarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!TryGetInputs(out double a, out double b))
+                return;
+
+            double suma = a + b;
+            lblResultado.Text = suma.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void restarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!TryGetInputs(out double a, out double b))
+                return;
+
+            double resta = a - b;
+            lblResultado.Text = resta.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void multiplicarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!TryGetInputs(out double a, out double b))
+                return;
+
+            double multi = a * b;
+            lblResultado.Text = multi.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void divisiónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!TryGetInputs(out double a, out double b))
+                return;
+
+            if (b == 0)
+            {
+                MessageBox.Show("No se puede dividir entre cero.");
+                return;
+            }
+
+            double division = a / b;
+            lblResultado.Text = division.ToString(CultureInfo.CurrentCulture);
+        }
+    }
+}
